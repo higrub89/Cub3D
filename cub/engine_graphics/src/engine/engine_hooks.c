618f1b8c4a2cd0824061f05613dd10ea1d341d7b
Ruben Dario Higuita Rubio <higuitaruben@hotmail.com>
2025-11-20 02:42:42 +0100

    Implementacion de los movimientos

--- cub/engine_graphics/src/engine/engine_hooks.c
+++ cub/engine_graphics/src/engine/engine_hooks.c
@@ -1,23 +1,23 @@
-#include "../include/cube3D.h"
+/* ************************************************************************** */
+/* */
+/* engine_hooks.c                                      :::      ::::::::    */
+/* By: Rubén <cub3D>                                 :+:      :+:    :+:    */
+/* +:+ +:+         +:+      */
+/* Created: 2025/02/20 12:00:00 by Rubén             #+#    #+#             */
+/* Updated: 2025/02/20 12:00:00 by Rubén            ###   ########.fr       */
+/* */
+/* ************************************************************************** */
 
-/**
- * @brief (FASE 2) La función de salida limpia.
- * Destruye todo lo de MLX y libera la memoria.
- */
+#include "../../include/cube3D.h"
+
+#define MOVE_SPEED 0.1
+#define ROT_SPEED 0.1
+
 int	ft_close_game(t_grl *grl)
 {
 	printf("Cerrando el juego...\n");
-	
-	// 1. Destruir el buffer de pantalla (FASE 3)
 	if (grl->engine.screen_buff.img_ptr)
 		mlx_destroy_image(grl->engine.mlx, grl->engine.screen_buff.img_ptr);
-	
-	// 2. (Aquí liberarías texturas cuando las carguemos)
-	// for (int i = 0; i < 4; i++)
-	//     if (grl->engine.textures[i].img_ptr)
-	//         mlx_destroy_image(grl->engine.mlx, grl->engine.textures[i].img_ptr);
-
-	// 3. Destruir la ventana y la conexión
 	if (grl->engine.win)
 		mlx_destroy_window(grl->engine.mlx, grl->engine.win);
 	if (grl->engine.mlx)
@@ -25,45 +25,69 @@ int	ft_close_game(t_grl *grl)
 		mlx_destroy_display(grl->engine.mlx);
 		free(grl->engine.mlx);
 	}
-	
-	// 4. Liberar datos mock (rutas de texturas y mapa)
 	ft_free_mock_data(grl);
-	
-	// 5. (Aquí llamarías a la función de Álex para liberar el parser)
-	// ft_free(grl); 
-
-	printf("¡Hasta luego!\n");
 	exit(0);
 	return (0);
 }
 
-/**
- * @brief (FASE 2) Manejador de pulsaciones de teclas.
- */
-static int	ft_handle_keypress(int keycode, t_grl *grl)
+static void	ft_rotate_player(t_grl *grl, double rot_speed)
 {
-	// Si se pulsa ESC, cerramos el juego
-	if (keycode == KEY_ESC)
+	double	old_dir_x;
+	double	old_plane_x;
+
+	old_dir_x = grl->player.dir_x;
+	grl->player.dir_x = grl->player.dir_x * cos(rot_speed)
+		- grl->player.dir_y * sin(rot_speed);
+	grl->player.dir_y = old_dir_x * sin(rot_speed)
+		+ grl->player.dir_y * cos(rot_speed);
+	old_plane_x = grl->player.plane_x;
+	grl->player.plane_x = grl->player.plane_x * cos(rot_speed)
+		- grl->player.plane_y * sin(rot_speed);
+	grl->player.plane_y = old_plane_x * sin(rot_speed)
+		+ grl->player.plane_y * cos(rot_speed);
+}
+
+static void	ft_move_player(t_grl *grl, double move_x, double move_y)
+{
+	int		map_x;
+	int		map_y;
+
+	map_x = (int)(grl->player.pos_x + move_x);
+	map_y = (int)(grl->player.pos_y);
+	if (grl->map.grid[map_y][map_x] != '1')
+		grl->player.pos_x += move_x;
+	map_x = (int)(grl->player.pos_x);
+	map_y = (int)(grl->player.pos_y + move_y);
+	if (grl->map.grid[map_y][map_x] != '1')
+		grl->player.pos_y += move_y;
+}
+
+static int	ft_handle_keypress(int key, t_grl *grl)
+{
+	if (key == KEY_ESC)
 		ft_close_game(grl);
-
-	// (Aquí irá la lógica de W, A, S, D y flechas)
-
+	if (key == KEY_W)
+		ft_move_player(grl, grl->player.dir_x * MOVE_SPEED,
+			grl->player.dir_y * MOVE_SPEED);
+	if (key == KEY_S)
+		ft_move_player(grl, -grl->player.dir_x * MOVE_SPEED,
+			-grl->player.dir_y * MOVE_SPEED);
+	if (key == KEY_A)
+		ft_move_player(grl, grl->player.dir_y * MOVE_SPEED,
+			-grl->player.dir_x * MOVE_SPEED);
+	if (key == KEY_D)
+		ft_move_player(grl, -grl->player.dir_y * MOVE_SPEED,
+			grl->player.dir_x * MOVE_SPEED);
+	if (key == KEY_RIGHT)
+		ft_rotate_player(grl, ROT_SPEED);
+	if (key == KEY_LEFT)
+		ft_rotate_player(grl, -ROT_SPEED);
 	return (0);
 }
 
-/**
- * @brief (FASE 2) Configura todos los hooks (eventos) de MLX.
- */
 void	ft_setup_hooks(t_grl *grl)
 {
-	// 1. Hook para cerrar con 'ESC'
 	mlx_key_hook(grl->engine.win, ft_handle_keypress, grl);
-
-	// 2. Hook para cerrar con la 'X' de la ventana
-	// (17 = DestroyNotify)
 	mlx_hook(grl->engine.win, 17, (1L << 0), ft_close_game, grl);
-
-	// 3. Hook para el loop de renderizado (FASE 3)
-	// Se ejecuta en cada frame antes de que MLX procese eventos
 	mlx_loop_hook(grl->engine.mlx, ft_game_loop, grl);
 }
